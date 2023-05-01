83bfbceb79b7a21c226d075308e6bce7e6c63778
thoak90codes <adorahtho@gmail.com>
2023-04-30 23:53:10 -0500

    added questions to index.js and to function init.

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,69 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    message: 'What is your GitHub username?',
+    name: 'github',
+  },
+  {
+    type: 'input',
+    message: 'What is your email address?',
+    name: 'email',
+  },
+  {
+    type: 'input',
+    message: 'What is the title of your project?',
+    name: 'title',
+  },
+  {
+    type: 'input',
+    message: 'Please provide a short description for your project.',
+    name: 'description',
+  },
+  {
+    type: 'input',
+    message: 'What command should be run to install dependencies?',
+    name: 'installation',
+  },
+  {
+    type: 'input',
+    message: 'How is this project used?',
+    name: 'usage',
+  },
+  {
+    type: 'input',
+    message: 'What kind of license should your project have?',
+    name: 'license',
+  },
+  {
+    type: 'input',
+    message: 'Please provide guidelines for contributing to the repo.',
+    name: 'contributing',
+  },
+  {
+    type: 'input',
+    message: 'What command should be run for tests?',
+    name: 'tests',
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {
+//   const readmeTemplate = `#
+//   `
+// }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions)
+  .then((answers) => {
+    console.log(answers)
+  })
+}
 
 // Function call to initialize app
 init();
