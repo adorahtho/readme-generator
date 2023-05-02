4a9c8b1b440132a79ef3e6d64a97476baaf31b28
thoak90codes <adorahtho@gmail.com>
2023-05-01 23:32:54 -0500

    added layout to readmeTemplate

--- Develop/index.js
+++ Develop/index.js
@@ -52,10 +52,45 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {
-//   const readmeTemplate = `#
-//   `
-// }
+function writeToFile(fileName, data) {
+  const readmeTemplate = `#${questions.title}
+
+  ## Description
+  ${questions.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  * [License](#license)
+
+  * [Contributing](#contributing)
+
+  * [Tests](#tests)
+
+  * [Questions](#questions)
+
+  ## Installation
+  To install the necessary dependencies, run the following command:
+  ${questions.installation}
+
+  ## Usage
+  ${questions.usage}
+
+  ## License
+  This project is licensd under the ${questions.license} license.
+
+  ## Contributing
+  ${questions.contributing}
+
+  ## Tests
+  To run tests, run the following command:
+  ${questions.tests}
+
+  ## Questions
+  If you have any questions about the repo, open an issue or contact me at ${questions.email}. You can find more of my work at ${questions.github}.`
+}
 
 // TODO: Create a function to initialize app
 function init() {
